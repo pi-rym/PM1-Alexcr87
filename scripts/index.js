78f48fd1ff66398995544ee119b667e740a30c93
Alexcr87 <alexcr87@live.com.ar>
2024-03-19 12:06:25 -0300

    funciona todo

--- scripts/index.js
+++ scripts/index.js
@@ -28,23 +28,45 @@ class Repository {
 }
 const repositorio = new Repository()
 
-function copiar(){
+function handlerButton(){
     const inputActividad = document.getElementById("inputActividad");               // getElementById es para seleccionar un elemento del DOM
     const textareaDescripcion = document.getElementById("textareaDescripcion");
     const inputImagen = document.getElementById("inputImagen");
-    title = inputActividad.value;
-    description = textareaDescripcion.value;
-    imgUrl = inputImagen.value;
-    repositorio.createActivity(title, description, imgUrl)
+    
+    const titleValue = inputActividad.value
+    const descriptionValue = textareaDescripcion.value
+    const imgValue = inputImagen.value
+
+    if (!titleValue || !descriptionValue || !imgValue){
+        alert ("Por favor llene todos los campos")
+        return
+    }
+    repositorio.createActivity(titleValue, descriptionValue, imgValue)
+    console.log(repositorio.activities)
+    convertAllActivities()
+}
+
+function convertAllActivities(){
+    const container = document.getElementById("contenedor")
+    container.innerHTML = ""
+    const activities = repositorio.getAllActivities()
+    const htmlActivities = activities.map((activity) => createHTMLActivity(activity))
+    console.log(htmlActivities)
+    htmlActivities.forEach(activityHTML => {container.appendChild(activityHTML)})
+}
+
+function createHTMLActivity(activity){
+    const {id, title, description, imgUrl} = activity
 
     const divContenedor = document.createElement("div");
     divContenedor.classList.add("nuevoDiv");
+    divContenedor.id = id
+    
 
     const htmlTitle = document.createElement("p")
     htmlTitle.innerHTML = title
     htmlTitle.classList.add("nuevaActividad")
     divContenedor.appendChild(htmlTitle)
-
     
     const htmlImg= document.createElement("img")
     htmlImg.src = imgUrl
@@ -58,12 +80,24 @@ function copiar(){
 
     const contenedor = document.getElementById("contenedor")
     contenedor.appendChild(divContenedor)
+    
+    const htmlbutton = document.createElement("button")
+    htmlbutton.dataset.id = id
+    htmlbutton.innerHTML = "Borrar"
+    divContenedor.appendChild(htmlbutton)
+    htmlbutton.addEventListener("click", borrarActividad)
+    
+    return divContenedor
 }
 
-
 const agregar = document.getElementById("agregar");
-agregar.addEventListener("click",copiar)
+agregar.addEventListener("click",handlerButton)
 
+function borrarActividad(event) {
+    const id = event.target.dataset.id
+    repositorio.deleteActivity(parseInt(id));
+    convertAllActivities();
+}
 
 
 
